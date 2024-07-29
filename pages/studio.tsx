6dfbae5a82da64b324d74e374fb45c9678e254e7
Backseats <gm@backseats.com>
2024-07-29 00:18:48 -0400

    button refac

--- pages/studio.tsx
+++ pages/studio.tsx
@@ -366,19 +366,6 @@ const Grid: React.FC = () => {
               >
                 Reset Canvas
               </button>
-
-              <button
-                onClick={printGrid}
-                className="px-4 py-2 bg-blue-500 text-white rounded hover:brightness-[70%] transition-colors"
-              >
-                Console.log Colormap
-              </button>
-              <button
-                onClick={copyTextAreaContent}
-                className="px-4 py-2 bg-green-500 text-white rounded hover:brightness-[70%] transition-colors"
-              >
-                Copy Colormap
-              </button>
             </div>
           </div>
         </div>
@@ -428,35 +415,53 @@ const Grid: React.FC = () => {
       <div className="ml-8 my-8 flex flex-row">
         <div className="w-1/2">
           <h3 className="text-xl font-semibold mb-2">Text Editor</h3>
-          <textarea
-            ref={textareaRef}
-            value={textAreaContent}
-            onChange={handleTextAreaChange}
-            className="w-8/12 h-[400px] p-2 border border-gray-300 rounded"
-            placeholder="Grid data in JSON format"
-          />
+          <div className="w-8/12 flex flex-col gap-2">
+            <textarea
+              ref={textareaRef}
+              value={textAreaContent}
+              onChange={handleTextAreaChange}
+              className=" h-[400px] p-2 border border-gray-300 rounded"
+              placeholder="Grid data in JSON format"
+            />
+
+            <div className="flex gap-2 mt-2 justify-center">
+              <button
+                onClick={copyTextAreaContent}
+                className="px-4 py-2 bg-green-500 text-white rounded hover:brightness-[70%] transition-colors"
+              >
+                Copy Colormap
+              </button>
+              <button
+                onClick={printGrid}
+                className="px-4 py-2 bg-blue-500 text-white rounded hover:brightness-[70%] transition-colors"
+              >
+                Console.log Colormap
+              </button>
+            </div>
+          </div>
         </div>
 
         <div className="max-w-1/2">
           <h3 className="text-xl font-semibold mb-2">SVG Preview</h3>
-          <div className="flex flex-col gap-2"></div>
-          <div
-            className="border border-gray-300"
-            dangerouslySetInnerHTML={{ __html: svgContent }}
-          />
-          <div className="flex gap-2 mt-2 justify-center">
-            <button
-              onClick={copySVGText}
-              className="px-4 py-2 bg-orange-500 text-white rounded hover:brightness-[70%] transition-colors"
-            >
-              Copy SVG Text
-            </button>
-            <button
-              onClick={downloadSVG}
-              className="px-4 py-2  bg-purple-500 text-white rounded hover:brightness-[70%] transition-colors"
-            >
-              Download SVG
-            </button>
+          <div className="flex flex-col gap-2">
+            <div
+              className="border border-gray-300"
+              dangerouslySetInnerHTML={{ __html: svgContent }}
+            />
+            <div className="flex gap-2 mt-2 justify-center">
+              <button
+                onClick={copySVGText}
+                className="px-4 py-2 bg-orange-500 text-white rounded hover:brightness-[70%] transition-colors"
+              >
+                Copy SVG Text
+              </button>
+              <button
+                onClick={downloadSVG}
+                className="px-4 py-2  bg-purple-500 text-white rounded hover:brightness-[70%] transition-colors"
+              >
+                Download SVG
+              </button>
+            </div>
           </div>
         </div>
       </div>
