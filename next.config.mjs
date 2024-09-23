75944de5682efb9cbf56112e2628c4019dceb620
Backseats <gm@backseats.com>
2024-09-22 23:54:37 -0400

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
+      'json-colorizer': 'commonjs json-colorizer'
+    })
+
+    config.module.rules.push({
+      test: /color-json\.ts$/,
+      loader: 'ignore-loader'
+    })
+
+    return config
+  },
 };
 
 export default nextConfig;
