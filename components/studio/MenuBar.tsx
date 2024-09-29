873e5395a3ea06fd4e9effcfed4878674d3267a9
Backseats <gm@backseats.com>
2024-09-29 19:53:13 -0400

    Update MenuBar.tsx

--- components/studio/MenuBar.tsx
+++ components/studio/MenuBar.tsx
@@ -12,7 +12,14 @@ export default function MenuBar(props: Props) {
 
   return (
     <div className="px-8 py-4 border-b border-gray-300 flex flex-col md:flex-row justify-between">
-      <p className="text-5xl md:text-3xl font-bold">Chonks Studio</p>
+      <div className="flex items-center gap-2">
+        <h1 className="text-5xl md:text-3xl font-bold cursor-pointer">
+          Chonks Studio
+        </h1>
+        <span className="text-sm font-normal text-red-400 relative -left-3 -top-3 rotate-12">
+          (beta)
+        </span>
+      </div>
 
       <div className="flex flex-col md:flex-row gap-4">
         <button
@@ -29,6 +36,8 @@ export default function MenuBar(props: Props) {
           Load a Trait
         </button>
 
+        <div className="w-[0.5px] bg-gray-500" />
+
         <button
           onClick={resetGrid}
           className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-red-500 transition-colors"
@@ -42,6 +51,8 @@ export default function MenuBar(props: Props) {
         >
           Reset Saved Colors
         </button>
+
+        <div className="w-[0.5px] bg-gray-500" />
 
         <ConnectKitButton />
       </div>
