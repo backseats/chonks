4e5faecb1e0081ff16bc8c9af2f9d856cd91627b
Mark A Lowe <marka@Marks-MacBook-Pro.local>
2024-10-08 21:43:22 +1100

    index fix

--- contracts/src/PetersMain.sol
+++ contracts/src/PetersMain.sol
@@ -164,7 +164,7 @@ contract PetersMain is IPeterStorage, IERC165, ERC721Enumerable, Ownable, IERC49
         peter.topId = traitsIds[2]; 
 
         // so we're not going to equip these on initial mint, people will have to equip them
-        // if(amount > 3) peter.hairId = traitsIds[3]; 
+        if(amount > 3) peter.hairId = traitsIds[3]; 
         // if(amount > 4) peter.faceId = traitsIds[4]; 
         // if(amount > 5) peter.hatId = traitsIds[5]; 
         // if(amount > 6) peter.accessoryId = traitsIds[6]; 
