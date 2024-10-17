5268927ebed06a42c74c7f5c79d7e19073a39fc6
Backseats <gm@backseats.com>
2024-10-17 17:37:37 -0400

    font stuff

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,500&family=Source+Code+Pro:wght@500&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,700;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
