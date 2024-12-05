1e7cb8a40c77858d4eaf621109963a581db97383
Backseats <gm@backseats.com>
2024-12-04 19:36:02 -0500

    latest bs contracts

--- contract_data.ts
+++ contract_data.ts
@@ -3,9 +3,9 @@ import traitsAbiImport from './contracts/out/ChonkTraits.sol/ChonkTraits.json'
 import marketplaceAbiImport from './contracts/out/ChonksMarket.sol/ChonksMarket.json'
 import { baseSepolia, base } from "viem/chains";
 
-export const mainContract = "0xeEd47d1c0702F146b242E91569E0A711d65ad229";
-export const traitsContract = "0xD4Ae162ACA47C44E033E489a100f20B2751bb232";
-export const marketplaceContract = "0x4577E6E9436E63b1789ADb70eC39B4211BFCb579";
+export const mainContract = "0x6e2753caaa1d0aac6ff43918bf2b7eef32768938";
+export const traitsContract = "0x6D72C979263aa373f5F21836c1D8Adf086D5850a";
+export const marketplaceContract = "0x4577E6E9436E63b1789ADb70eC39B4211BFCb579"; // needs updating
 
 export const mainABI = mainAbiImport.abi;
 export const traitsABI = traitsAbiImport.abi;
