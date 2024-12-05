77fc2a712b02c515e09c6b74dca2d42b2cd0ce38
Backseats <gm@backseats.com>
2024-12-05 00:17:23 -0500

    Revert "walletconnect"
    This reverts commit ffbe8b47c3ae38084f7011fcf18de0f5693ce3e1.

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 import { http, createConfig } from "wagmi";
-import { base } from "wagmi/chains";
-import { coinbaseWallet, walletConnect } from "wagmi/connectors";
+import { baseSepolia, base } from "wagmi/chains";
+import { coinbaseWallet } from "wagmi/connectors";
 
 // Only for local dev
 // export const publicClient = createPublicClient({
@@ -15,7 +15,6 @@ export const config = createConfig({
   chains: [base],
   connectors: [
     coinbaseWallet({ appName: 'Chonks', preference: 'all' }),
-    walletConnect({ projectId: "6637dd8a880463f857799d3d1011b7a2"})
   ],
   transports: {
     [base.id]: http(`${process.env.NEXT_PUBLIC_ALCHEMY_BASE_MAINNET_RPC_URL}`),
