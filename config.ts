c44137ee137da1c558e98a6ef4350e8ff1565d0f
Backseats <gm@backseats.com>
2025-03-14 12:07:39 -0400

    config prep

--- config.ts
+++ config.ts
@@ -8,8 +8,11 @@ import { chonksMarketABI } from './abis/chonksMarketABI'
 
 export const mainContract = "0x07152bfde079b5319e5308c43fb1dbc9c76cb4f9";
 
-export const traitsContract = "0xc86adB45e13366635902a002C03ef3a748969595"; // "0x6b8f34e0559aa9a5507e74ad93374d9745cdbf09";
-export const marketplaceContract = "0x74D8725A65C21251A83f6647aa23140Bd80504b1"; // "0xf127467f1e94593b1606bf0da3d08e3c15b2b291";
+// 0x6b8f34e0559aa9a5507e74ad93374d9745cdbf09 old traits contract
+export const traitsContract = "";
+
+// 0xf127467f1e94593b1606bf0da3d08e3c15b2b291 old marketplace contract
+export const marketplaceContract = "";
 
 export const mainABI = chonksMainABI;
 export const traitsABI = chonkTraitsABI;
@@ -23,13 +26,13 @@ export const config = createConfig({
   chains: [base],
   connectors: [
     coinbaseWallet({ appName: 'Chonks', preference: 'all' }),
-    // walletConnect({
-    //   projectId: "6637dd8a880463f857799d3d1011b7a2",
-    // }),
-    // injected()
+    walletConnect({
+      projectId: "6637dd8a880463f857799d3d1011b7a2",
+    }),
+    injected()
   ],
   transports: {
-    // [base.id]: http(`${process.env.NEXT_PUBLIC_ALCHEMY_BASE_MAINNET_RPC_URL}`), // toggle for prod
-    [base.id]: http('http://localhost:8545') // toggle for dev
+    [base.id]: http(`${process.env.NEXT_PUBLIC_ALCHEMY_BASE_MAINNET_RPC_URL}`), // toggle for prod
+    // [base.id]: http('http://localhost:8545') // toggle for dev
   },
 });
