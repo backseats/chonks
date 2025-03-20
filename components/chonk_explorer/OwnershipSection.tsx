1d036e90b676b13d8e44149758bc67fb08b4856c
Backseats <gm@backseats.com>
2025-03-20 13:25:37 -0400

    font size fixes

--- components/chonk_explorer/OwnershipSection.tsx
+++ components/chonk_explorer/OwnershipSection.tsx
@@ -39,14 +39,16 @@ export default function OwnershipSection(props: Props) {
 
   return (
     <>
-      <h1 className="text-2xl text-center mt-6 mb-1">Chonk #{id}</h1>
+      <h1 className="text-[28px] font-bold text-center mt-6 mb-1">
+        Chonk #{id}
+      </h1>
 
       {/* TODO: ENS for owner, heads up on the network, might need to use mainnet ens as well as basename */}
       {owner && (
-        <div className="w-full flex justify-center mb-4 font-bold">
+        <div className="w-full flex justify-center mb-4">
           <div className="flex flex-col sm:flex-row items-center">
             <div
-              className={`text-lg underline cursor-pointer sm:mt-4 mt-4 mb-2 sm:mb-0`}
+              className={`text-[16px] sm:text-lg underline cursor-pointer sm:mt-4 mt-4 mb-2 sm:mb-0`}
               onClick={goToOwnerProfile}
             >
               {isYours
