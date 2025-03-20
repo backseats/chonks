446579f8a038890f22992428d0bfbf0d2d642159
Backseats <gm@backseats.com>
2025-03-20 12:10:57 -0400

    colormap address and abi

---
+++ abis/chonksColorMapABI.ts
@@ -0,0 +1,59 @@
+export const chonksColorMapABI = [
+{
+  "inputs": [
+    {
+      "internalType": "uint256",
+      "name": "_chonkId",
+      "type": "uint256"
+    }
+  ],
+  "name": "getColorMapForChonk",
+  "outputs": [
+    {
+      "internalType": "bytes",
+      "name": "",
+      "type": "bytes"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [
+    {
+      "internalType": "uint256",
+      "name": "_chonkId",
+      "type": "uint256"
+    }
+  ],
+  "name": "getBodyIndexForChonk",
+  "outputs": [
+    {
+      "internalType": "uint8",
+      "name": "",
+      "type": "uint8"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [
+    {
+      "internalType": "uint256",
+      "name": "_chonkId",
+      "type": "uint256"
+    }
+  ],
+  "name": "getBackgroundColorForChonk",
+  "outputs": [
+    {
+      "internalType": "string",
+      "name": "",
+      "type": "string"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+}
+]
--- config.ts
+++ config.ts
@@ -5,6 +5,7 @@ import { base } from "viem/chains";
 import { chonksMainABI } from './abis/chonksMainABI'
 import { chonkTraitsABI } from './abis/chonkTraitsABI'
 import { chonksMarketABI } from './abis/chonksMarketABI'
+import { chonksColorMapABI } from './abis/chonksColorMapABI'
 
 export const mainContract = "0x07152bfde079b5319e5308c43fb1dbc9c76cb4f9";
 
@@ -14,9 +15,12 @@ export const traitsContract = "0x74D8725A65C21251A83f6647aa23140Bd80504b1";
 // 0xf127467f1e94593b1606bf0da3d08e3c15b2b291 old marketplace contract
 export const marketplaceContract = "0x6d00a9A2a0C6B5499d56bd4c9005663C88a544a6";
 
+export const colorMapContract = "0x92BC112321E1EEd44C7CdB802ED727Ef2a9864Cd";
+
 export const mainABI = chonksMainABI;
 export const traitsABI = chonkTraitsABI;
 export const marketplaceABI = chonksMarketABI;
+export const colorMapABI = chonksColorMapABI;
 
 export const simpleHashKey = "makingmemark_sk_ab7284sr3v5o19g3iaublzep6sffynfu"; // TODO: env var
 
