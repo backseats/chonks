1156ab9c18ea18d203e40d8f0a6ec21ce6248486
Backseats <gm@backseats.com>
2025-03-28 19:45:41 -0400

    fix key error in Tabs

--- components/marketplace/Tabs.tsx
+++ components/marketplace/Tabs.tsx
@@ -30,22 +30,15 @@ export default function Tabs({ activeTab, onTabChange }: TabsProps) {
     <section className="tabs text-[14px] border-t border-gray-300  flex flex-col bg-white py-[1.725vw] px-[3.45vw]">
       <div className="flex">
         {tabs.map((tab, index) => (
-          <>
-            <button
-              key={`${tab}-${index}`}
-              className={`hover:bg-gray-200 hover:text-black px-4 mr-2 py-2 border border-black ${
-                activeTab === tab ? "bg-[#126E9D] text-white" : ""
-              }`}
-              onClick={() => handleTabClick(tab)}
-            >
-              {tab}
-            </button>
-
-            {/* vert divider */}
-            {/* {index === 1 && (
-              <div className="h-8 w-px bg-gray-300 mx-[1.725vw] self-center" />
-            )} */}
-          </>
+          <button
+            key={index}
+            className={`hover:bg-gray-200 hover:text-black px-4 mr-2 py-2 border border-black ${
+              activeTab === tab ? "bg-[#126E9D] text-white" : ""
+            }`}
+            onClick={() => handleTabClick(tab)}
+          >
+            {tab}
+          </button>
         ))}
       </div>
     </section>
