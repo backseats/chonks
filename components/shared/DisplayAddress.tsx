bb1441a58209aee7f6f53fef18f169e9d35824fe
Mark A Lowe <makingmemark@gmail.com>
2025-04-11 15:15:34 +1000

    DisplayAddress now links to profile/xxxx

--- components/shared/DisplayAddress.tsx
+++ components/shared/DisplayAddress.tsx
@@ -1,11 +1,14 @@
 import { useEnsName } from "wagmi";
 import { mainnet } from "wagmi/chains";
 import { Address } from "viem";
+import Link from "next/link";
 import { truncateEthAddress } from "@/utils/truncateEthAddress";
 
 interface DisplayAddressProps {
   address: Address;
 }
+
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 export default function DisplayAddress({ address }: DisplayAddressProps) {
   const { data: ensName } = useEnsName({
@@ -13,5 +16,15 @@ export default function DisplayAddress({ address }: DisplayAddressProps) {
     chainId: mainnet.id,
   });
 
-  return <span>{ensName || truncateEthAddress(address)}</span>;
+  const displayValue = ensName || truncateEthAddress(address);
+
+  if (address === ZERO_ADDRESS) {
+    return <span>{displayValue}</span>;
+  }
+
+  return (
+    <Link href={`/profile/${address}`} className="hover:underline">
+      {displayValue}
+    </Link>
+  );
 }
